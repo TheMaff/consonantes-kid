34e5e5f97759f6d39d5eea0a54abf37d1d945cad
Marcos Flores <maff.dsgn@gmail.com>
2026-04-27 17:27:58 -0400

    Fix: delete Rect import cause is declared but its value is never read

--- src/components/WordChain.tsx
+++ src/components/WordChain.tsx
@@ -1,5 +1,5 @@
 // src/components/WordChain.tsx
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Box, Flex, Text, Button } from "@chakra-ui/react";
 import { keyframes } from "@emotion/react";
 import {
